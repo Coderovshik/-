4fa95e4c59b495a2d74eb8087e7e7202b97251b2
Coderovshik <peter.04007@gmail.com>
2023-11-22 17:36:37 +0300

    init

---
+++ scripts/library.js
@@ -0,0 +1,185 @@
+let selections = [
+    {
+        title: 'Все книги',
+        description: '532 000 книг',
+        books: [
+            {
+                cover: '../assets/images/cover1.jpg',
+                rating: 4,
+                title: 'Переговоры за минуту. Экспресс курс делового общения',
+                author: 'Эдуард Трымбовецкий',
+                to: '../pages/book.html'
+            },
+            {
+                cover: '../assets/images/cover2.jpg',
+                rating: 5,
+                title: 'Переговоры за минуту. Экспресс-курс делового общения',
+                author: 'Эдуард Трымбовецкий',
+                to: '../pages/book.html'
+            },
+            {
+                cover: '../assets/images/cover3.jpg',
+                rating: 4,
+                title: 'Переговоры за минуту. Экспресс-курс делового общения',
+                author: 'Эдуард Трымбовецкий',
+                to: '../pages/book.html'
+            },
+            {
+                cover: '../assets/images/cover4.jpg',
+                rating: 3,
+                title: 'Переговоры за минуту. Экспресс-курс делового общения',
+                author: 'Эдуард Трымбовецкий',
+                to: '../pages/book.html'
+            },
+            {
+                cover: '../assets/images/cover5.jpg',
+                rating: 4,
+                title: 'Переговоры за минуту. Экспресс-курс делового общения',
+                author: 'Эдуард Трымбовецкий',
+                to: '../pages/book.html'
+            },
+            {
+                cover: '../assets/images/cover6.jpg',
+                rating: 4,
+                title: 'Переговоры за минуту. Экспресс-курс делового общения',
+                author: 'Эдуард Трымбовецкий',
+                to: '../pages/book.html'
+            },
+            {
+                cover: '../assets/images/cover7.jpg',
+                rating: 3,
+                title: 'Переговоры за минуту. Экспресс-курс делового общения',
+                author: 'Эдуард Трымбовецкий',
+                to: '../pages/book.html'
+            },
+            {
+                cover: '../assets/images/cover8.jpg',
+                rating: 4,
+                title: 'Переговоры за минуту. Экспресс-курс делового общения',
+                author: 'Эдуард Трымбовецкий',
+                to: '../pages/book.html'
+            },
+        ],
+    },
+    {
+        title: 'Все аудиокниги',
+        description: '208 000 аудиокниг',
+        books: [
+            {
+                cover: '../assets/images/audio-cover.jpg',
+                rating: 4,
+                title: 'Илон Маск. Уолтер Айзексон. Саммари',
+                author: 'Smart Reading',
+                to: '../pages/book.html'
+            },
+            {
+                cover: '../assets/images/audio-cover.jpg',
+                rating: 4,
+                title: 'Илон Маск. Уолтер Айзексон. Саммари',
+                author: 'Smart Reading',
+                to: '../pages/book.html'
+            },
+            {
+                cover: '../assets/images/audio-cover.jpg',
+                rating: 4,
+                title: 'Илон Маск. Уолтер Айзексон. Саммари',
+                author: 'Smart Reading',
+                to: '../pages/book.html'
+            },
+            {
+                cover: '../assets/images/audio-cover.jpg',
+                rating: 4,
+                title: 'Илон Маск. Уолтер Айзексон. Саммари',
+                author: 'Smart Reading',
+                to: '../pages/book.html'
+            },
+            {
+                cover: '../assets/images/audio-cover.jpg',
+                rating: 4,
+                title: 'Илон Маск. Уолтер Айзексон. Саммари',
+                author: 'Smart Reading',
+                to: '../pages/book.html'
+            },
+        ]
+    },
+    {
+        title: 'Авторы',
+        description: '128 470 авторов',
+        books: [
+            {
+                cover: '../assets/images/author-cover.jpg',
+                rating: 0,
+                title: 'Стивен Кинг',
+                author: '17 книг',
+                to: '../pages/book.html'
+            },
+            {
+                cover: '../assets/images/author-cover.jpg',
+                rating: 0,
+                title: 'Стивен Кинг',
+                author: '17 книг',
+                to: '../pages/book.html'
+            },
+            {
+                cover: '../assets/images/author-cover.jpg',
+                rating: 0,
+                title: 'Стивен Кинг',
+                author: '17 книг',
+                to: '../pages/book.html'
+            },
+            {
+                cover: '../assets/images/author-cover.jpg',
+                rating: 0,
+                title: 'Стивен Кинг',
+                author: '17 книг',
+                to: '../pages/book.html'
+            },
+            {
+                cover: '../assets/images/author-cover.jpg',
+                rating: 0,
+                title: 'Стивен Кинг',
+                author: '17 книг',
+                to: '../pages/book.html'
+            },
+            {
+                cover: '../assets/images/author-cover.jpg',
+                rating: 0,
+                title: 'Стивен Кинг',
+                author: '17 книг',
+                to: '../pages/book.html'
+            },
+        ]
+    }
+];
+
+let selectionGroup = document.querySelector(".selection-group");
+selectionGroup.innerHTML = '';
+for (let selection of selections) {
+    let books = '';
+    for (let book of selection.books) {
+        books += `
+            <li class="book">
+                <a href=${book.to}><img class="book-cover" src="${book.cover}" alt=""></a>
+                <div class="book-rating">${"⭐".repeat(book.rating)}</div>
+                <a href=${book.to}><h5 class="book-title">${truncate(book.title, 32)}</h5></a>
+                <p class="book-author">${truncate(book.author, 12)}</p>
+            </li>`
+    }
+
+    selectionGroup.innerHTML += `
+        <div class="selection">
+            <h3 class="selection-title">${selection.title}</h3>
+            <p class="selction-description">${selection.description}</p>
+            <ul class="books">
+                ${books}
+            </ul>
+        </div>`;
+}
+
+function truncate(str, max) {
+    if (str.length < max) {
+        return str;
+    } else {
+        return str.substring(0, max) + '...';
+    }
+}
